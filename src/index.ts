420f3e0e16e640be02d850d804e6cf2d4557547f
Jayesh <en001507@gmail.com>
2024-07-10 18:42:57 +0530

    ensure connection to db first

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import config from "./config/config";
 import app from './app';
+import { prismaClient } from "./client/prisma";
 
 const PORT = config.port;
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+prismaClient.$connect()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Failed to connect to the database:', error);
+    process.exit(1); // Exit the process with failure
+  });
+
